f09d41c88f01b9c1353ccc3307c07f023dedac60
Your Name <srujanravi30@gmail.com>
2022-09-30 22:35:37 +0530

    changes

--- src/components/About.js
+++ src/components/About.js
@@ -67,9 +67,6 @@ export default function About() {
                 </div>
               </div>
             </div>
-            <div className='container my-3'>
-        <button onClick = {toggleStyle} type="button" className='btn btn-primary'>{btnText}</button>
-    </div>
     </div>
   )
 }
